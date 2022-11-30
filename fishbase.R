a0b4e98b9402c67848ccfe5b493f50d9c41bf9ed
elkewind <elke.windschitl@gmail.com>
2022-11-29 21:04:56 -0800

    some initial analysis

--- fishbase.R
+++ fishbase.R
@@ -192,8 +192,37 @@ mod <- glm(is_of_concern ~ length_cm + reef_associated + is_endemic,
            family = "binomial")
 mod
 
-b1 <- mod$coefficients[1]
-b2 <- mod$coefficients[2]
-b3 <- mod$coefficients[3]
-b4 <- mod$coefficients[4]
+# Pull out all coefficients
+b0 <- mod$coefficients[1] #Intercept
+b1 <- mod$coefficients[2] #Length
+b2 <- mod$coefficients[3] #Reef Associated
+b3 <- mod$coefficients[4] #Endemic
+
+# Run some test probabilities 
+equ <- b0 * b1 * 700  * b2 * b3
+p_700_1_1 <- (exp(equ)) / (1 + exp(equ))
+
+equ20 <- b0 * b1 * 20 * b2 * b3
+p_20_1_1 <- (exp(equ20)) / (1 + exp(equ20))
+
+equ450 <- b0 * b1 * 450 * b2 * b3
+p_450_1_1 <- (exp(equ450)) / (1 + exp(equ450))
+
+# Write a function for testing probabilities
+threat_prob <- function(b0, b1, b2, b3, len, reef, end) {
+  equ <- b0 * b1 * len * b2 * reef * b3 * end
+  prob <- (exp(equ)) / (1 + exp(equ))
+  print(prob)
+}
+# Test
+threat_prob(b0, b1, b2, b3, 700, 1, 1)
+threat_prob(b0, b1, b2, b3, 20, 1, 1)
+threat_prob(b0, b1, b2, b3, 450, 1, 1)
+
+# Attempt some t-tests???
+reef_test <- t.test(is_of_concern ~ reef_associated, data = tidy_fish_data)
+end_test <- t.test(is_of_concern ~ is_endemic, data = tidy_fish_data)
+
+
+
 
